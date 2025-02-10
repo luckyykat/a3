1f90c136b2a0c945874374c75e48a49a2d74e5f7
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2025-02-10 13:16:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 function generateRhombus() {
-    console.log("Script is running!"); // Debugging check
-
     let height = parseInt(document.getElementById("height").value);
     let colorOdd = document.getElementById("colorOdd").value;
     let colorEven = document.getElementById("colorEven").value;
     let symbol = document.getElementById("symbol").value;
 
-    console.log(`Height: ${height}, Colors: ${colorOdd}, ${colorEven}, Symbol: ${symbol}`);
-
     let outputDiv = document.getElementById("output");
-    if (!outputDiv) {
-        console.error("Error: Output div not found!");
-        return;
-    }
-
     outputDiv.innerHTML = ""; // Clear previous output
+
     let rhombusHTML = "";
 
     // Generate top half
     for (let i = 1; i <= height; i++) {
-        rhombusHTML += "<p style='text-align:center'>";
-        rhombusHTML += createLeftSide(i, height, colorOdd, colorEven, symbol);
-        rhombusHTML += createRightSide(i, colorOdd, colorEven, symbol);
+        rhombusHTML += "<p style='text-align:center;'>";
+        rhombusHTML += "&nbsp;".repeat(height - i); // Add spaces for centering
+        rhombusHTML += createRow(i, colorOdd, colorEven, symbol);
         rhombusHTML += "</p>";
     }
 
     // Generate bottom half
     for (let i = height - 1; i > 0; i--) {
-        rhombusHTML += "<p style='text-align:center'>";
-        rhombusHTML += createLeftSide(i, height, colorOdd, colorEven, symbol);
-        rhombusHTML += createRightSide(i, colorOdd, colorEven, symbol);
+        rhombusHTML += "<p style='text-align:center;'>";
+        rhombusHTML += "&nbsp;".repeat(height - i); // Add spaces for centering
+        rhombusHTML += createRow(i, colorOdd, colorEven, symbol);
         rhombusHTML += "</p>";
     }
+
+    outputDiv.innerHTML = rhombusHTML;
+}
+
+// Creates a row with alternating colors
+function createRow(row, colorOdd, colorEven, symbol) {
+    let rowHTML = "";
+    for (let j = 0; j < row * 2 - 1; j++) { // Multiply by 2 to make it symmetrical
+        let color = (j % 2 === 0) ? colorOdd : colorEven;
+        rowHTML += `<span style="color:${color};">${symbol}</span>`;
+    }
+    return rowHTML;
+}
 
     console.log("Rhombus generated successfully!");
     outputDiv.innerHTML = rhombusHTML;
